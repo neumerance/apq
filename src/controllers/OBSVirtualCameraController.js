71948c4bbcf1be7f7e9d3eda5b2e34f21cced2aa
JonJon <neumerance@live.com>
2025-04-11 04:35:42 +0800

    introduce OBSController

---
+++ src/controllers/OBSVirtualCameraController.js
@@ -0,0 +1,208 @@
+import os from 'os';
+import path from 'path';
+import { spawn } from 'child_process';
+import OBSWebSocket from "obs-websocket-js";
+import AppEmitter from '../helpers/AppEmitter';
+
+class OBSVirtualCameraController {
+  static PLATFORM = os.platform();
+  static DEFAULT_OBS_HEADLESS_ARGS = [
+    '--startvirtualcam',
+    '--minimize-to-tray',
+    '--no-browser'
+  ];
+  static DARWIN_OBS_PATH = '/Applications/OBS.app/Contents/MacOS/obs';
+  static WIN32_OBS_PATH = 'C:\\Program Files\\obs-studio\\bin\\64bit\\obs64.exe';
+  static LINUX_OBS_PATH = 'obs';
+  static WEBSOCKET = new OBSWebSocket('ws://localhost:4455');
+  static WEBSOCKET_URL = 'ws://localhost:4455';
+  static WEBSOCKET_PASS = 'ABC12abc';
+  static EMITTER = new AppEmitter();
+  static SCENE_NAME = 'apq-scene';
+  static INPUT_NAME = 'APQ Video Frames';
+  static TEMP_FILE = path.join(os.tmpdir(), "obs_temp_image.jpg");
+
+  contructor() {
+    this.obs = null;
+    this.socket = null;
+    this.sourceInitialized = false;
+  }
+
+  // Init methods start
+  async initOBSHeadless() {
+    try {
+      if (this.obs) return;
+
+      if (OBSVirtualCameraController.PLATFORM === 'darwin') {
+        this.obs = spawn(OBSVirtualCameraController.DARWIN_OBS_PATH, OBSVirtualCameraController.DEFAULT_OBS_HEADLESS_ARGS);
+      } else if (OBSVirtualCameraController.PLATFORM === 'win32') {
+        this.obs = spawn(OBSVirtualCameraController.WIN32_OBS_PATH, OBSVirtualCameraController.DEFAULT_OBS_HEADLESS_ARGS);
+      } else if (OBSVirtualCameraController.PLATFORM === 'linux') {
+        this.obs = spawn(OBSVirtualCameraController.LINUX_OBS_PATH, OBSVirtualCameraController.DEFAULT_OBS_HEADLESS_ARGS);
+      } else {
+        throw new Error("❌ Unsupported OS");
+      }
+
+      this.obs.stdout.setEncoding('utf8');
+      this.onWebsocketReady();
+      this.onOBSReady();
+      this.listenToOBSEvents();
+    } catch(e) {
+      console.log('❌ Unable to initialize OBS', e);
+      throw new Error('❌ Unable to initialize OBS');
+    }
+
+    return;
+  }
+
+  async initWebsocket() {
+    console.log('🚀 initiating obs websocket 🎬');
+
+    const websocketURL = OBSVirtualCameraController.WEBSOCKET_URL;
+    const websocketPassword = OBSVirtualCameraController.WEBSOCKET_PASS
+    this.socket = new OBSWebSocket(websocketURL);
+    this.socket.connect(websocketURL, websocketPassword).then(async (event) => {
+      console.log('⚡️ WebSocket is connected:', event);
+
+      OBSVirtualCameraController.EMITTER.emit('WEBSOCKET_READY');
+    })
+    .catch((error) => {
+      console.error('Failed to connect to OBS WebSocket:', error);
+    });
+  }
+
+  async initOBSSource() {
+    try {
+      const sceneExists = await this.isSceneExists();
+      if (!sceneExists) {
+        await this.createScene();
+        await this.createInput();
+      }
+
+      await this.setCurrentProgram();
+
+      this.sourceInitialized = true;
+    } catch (e) {
+      console.error('Unable to create OBS Source:', e);
+      this.sourceInitialized = false;
+    }
+  }
+  // Init methods end
+
+  // Listeners start
+  onOBSReady() {
+    OBSVirtualCameraController.EMITTER.on('OBS_READY', async () => {
+      await this.initWebsocket();
+    });
+  }
+
+  onWebsocketReady() {
+    OBSVirtualCameraController.EMITTER.on('WEBSOCKET_READY', async () => {
+      await this.initOBSSource();
+      await this.disableOBSStudioMode();
+      await this.toggleOBSVirtualCam();
+    });
+  }
+
+  listenToOBSEvents() {
+    this.obs.stdout.on('data', (data) => {
+      console.log(`OBS stdout: ${data}`);
+
+      if (data.includes('Camera Extension activated successfully')) {
+        console.log('✅ OBS Virtual Cam started, ready to go!');
+
+        OBSVirtualCameraController.EMITTER.emit('OBS_READY');
+      }
+    });
+
+    this.obs.stderr.on('data', (data) => {
+      console.error(`OBS stderr: ${data}`);
+    });
+
+    this.obs.on('close', (code) => {
+      console.log(`OS process exited with code ${code}`);
+    });
+  }
+  // Listerners end
+
+  // getters start
+  // getters end
+
+  // checkers start
+  async isSceneExists() {
+    const scenes = await this.socket.call('GetSceneList');
+    return scenes.scenes.some(scene => scene.sceneName === OBSVirtualCameraController.SCENE_NAME);
+  }
+  // checkers end
+
+  // methods start
+  async disableOBSStudioMode() {
+    await this.socket.call('SetStudioModeEnabled', {
+      studioModeEnabled: false
+    });
+  }
+
+  async toggleOBSVirtualCam(bool = true) {
+    if (!this.obs || !this.socket) return;
+
+    await this.socket.call('ToggleVirtualCam', {
+      outputActive: bool
+    });
+  }
+
+  async createScene() {
+    await this.socket.call('CreateScene', {
+      sceneName: OBSVirtualCameraController.SCENE_NAME
+    });
+  }
+
+  async createInput() {
+    await this.socket.call('CreateInput', {
+      sceneName: OBSVirtualCameraController.SCENE_NAME,
+      inputName: OBSVirtualCameraController.INPUT_NAME,
+      inputKind: 'image_source',
+      inputSettings: {
+        file: OBSVirtualCameraController.TEMP_FILE
+      }
+    });
+  }
+
+  async setCurrentProgram() {
+    await this.socket.call('SetCurrentProgramScene', {
+      sceneName: OBSVirtualCameraController.SCENE_NAME,
+    });
+  }
+
+  async setInputSettings(frame) {
+    await this.socket.call('SetInputSettings', {
+      inputName: OBSVirtualCameraController.INPUT_NAME,
+      inputKind: 'image_source',
+      inputSettings: {
+        file: frame
+      }
+    });
+  }
+
+  async receiveFrames(frame) {
+    if (!this.obs || !this.sourceInitialized) return;
+
+    try {
+      await this.setInputSettings(frame);
+    } catch(e) {
+      console.log('❌ Unable to receive frames', e);
+      throw new Error('❌ Unable to receive frames');
+    }
+  }
+
+  quit() {
+    if (!this.obs) return;
+
+    this.obs.kill('SIGTERM');
+    this.obs = null;
+    this.socket = null;
+    this.sourceInitialized = false;
+  }
+  // methods end
+}
+
+export default OBSVirtualCameraController;
--- src/main.js
+++ src/main.js
@@ -1,127 +1,8 @@
 import { app, BrowserWindow, screen, ipcMain } from 'electron'
 import path from 'path'
-import os from 'os';
-import pythonWebsocketServer from './pythonWebsocketServer.js';
-import { exec, spawn } from 'child_process';
-import OBSWebSocket from "obs-websocket-js";
+import OBSVirtualCameraController from './controllers/OBSVirtualCameraController.js';
 
-const platform = os.platform();
-// Define a fixed temporary file path for the vide frames
-const tempFilePath = path.join(os.tmpdir(), "obs_temp_image.jpg");
-let obsHeadless;
-let obsWebsocket;
-let obsWebsocketInitialized = false;
-
-const launchObsHeadless = async () => {
-  if (obsHeadless) {
-    if (obsWebsocket) {
-      await obsWebsocket.call('ToggleVirtualCam', {
-        outputActive: true
-      });
-    }
-
-    return;
-  }
-  // Launch OBS in headless mode
-  if (platform === 'darwin') {
-    // macOS
-    obsHeadless = spawn('/Applications/OBS.app/Contents/MacOS/obs', [
-      '--startvirtualcam',
-      '--minimize-to-tray',
-      '--no-browser'
-    ]);
-  } else if (platform === 'win32') {
-    // Windows - assuming default install path
-    obsHeadless = spawn('C:\\Program Files\\obs-studio\\bin\\64bit\\obs64.exe', [
-      '--startvirtualcam',
-      '--minimize-to-tray',
-      '--no-browser'
-    ]);
-  } else if (platform === 'linux') {
-    // Linux - assuming `obs` is in PATH
-    obsHeadless = spawn('obs', [
-      '--startvirtualcam',
-      '--minimize-to-tray',
-      '--no-browser'
-    ]);
-  } else {
-    alert('❌ Unsupported OS');
-  }
-  obsHeadless.stdout.setEncoding('utf8');
-
-  // Log any output from OBS for debugging
-  obsHeadless.stdout.on('data', (data) => {
-    console.log(`OBS stdout: ${data}`);
-
-    if (data.includes('Camera Extension activated successfully')) {
-      console.log('✅ OBS Virtual Cam started, ready to go!');
-
-      // You can now connect WebSocket or start your pipeline
-      console.log('🚀 initiating obs websocket 🎬');
-      initOBSWebsocket();
-    }
-  });
-
-  obsHeadless.stderr.on('data', (data) => {
-    console.error(`OBS stderr: ${data}`);
-  });
-
-  obsHeadless.on('close', (code) => {
-    console.log(`OS process exited with code ${code}`);
-  });
-}
-
-const initOBSWebsocket = () => {
-  obsWebsocket = new OBSWebSocket('ws://localhost:4455');
-  obsWebsocket.connect('ws://localhost:4455', 'ABC12abc').then(async (event) => {
-    console.log('⚡️ WebSocket is connected:', event);
-
-    await addCanvasAsSource(obsWebsocket);
-  })
-  .catch((error) => {
-    console.error('Failed to connect to OBS WebSocket:', error);
-  });
-}
-
-const addCanvasAsSource = async (socket) => {
-  try {
-    // First, check if the scene exists
-    const scenes = await socket.call('GetSceneList');
-    const sceneExists = scenes.scenes.some(scene => scene.sceneName === 'apq-scene');
-
-    await socket.call('SetStudioModeEnabled', {
-      studioModeEnabled: false
-    });
-    await socket.call('ToggleVirtualCam', {
-      outputActive: true
-    });
-
-    if (!sceneExists) {
-      // Now, proceed to create the input (source)
-      await socket.call('CreateScene', {
-        sceneName: 'apq-scene',  // Name of your scene
-      });
-
-      await socket.call('CreateInput', {
-        sceneName: 'apq-scene',  // Name of your scene
-        inputName: 'APQ Video Frames',  // Name of the source
-        inputKind: 'image_source',  // Use image source for static frames
-        inputSettings: {
-          file: tempFilePath  // Path to the image file
-        }
-      });
-    }
-
-    await socket.call('SetCurrentProgramScene', {
-      sceneName: 'apq-scene',
-    });
-
-    obsWebsocketInitialized = true;
-  } catch (e) {
-    console.error('Unable to create OBS Source:', e);
-    obsWebsocketInitialized = false;
-  }
-}
+const OBSController = new OBSVirtualCameraController();
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -183,19 +64,7 @@ ipcMain.on('frame-data', async (event, frame) => {
     canvasWindow.setFullScreen(true);
   }
 
-  if (obsWebsocket && obsWebsocketInitialized) {
-    try {
-      await obsWebsocket.call('SetInputSettings', {
-        inputName: 'APQ Video Frames',  // Name of the source
-        inputKind: 'image_source',  // Use image source for static frames
-        inputSettings: {
-          file: frame  // Path to the image file
-        }
-      });
-    } catch (e) {
-      console.error('Unable to update input settings', e);
-    }
-  }
+  OBSController.receiveFrames(frame);
 });
 
 // close canvas windows
@@ -211,15 +80,9 @@ ipcMain.handle('get-displays', () => {
 });
 
 ipcMain.handle('toggle-obs-virtual-cam', async (event, enableOBSVirtualCam) => {
-  if (enableOBSVirtualCam) {
-    await launchObsHeadless();
-  } else {
-    await obsWebsocket.call('ToggleVirtualCam', {
-      outputActive: false
-    });
-  }
+  if (enableOBSVirtualCam) await OBSController.initOBSHeadless();
+  await OBSController.toggleOBSVirtualCam(enableOBSVirtualCam)
 });
-
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
@@ -227,8 +90,5 @@ app.on('window-all-closed', () => {
 
 // Gracefully quit OBS when Electron app is closed
 app.on('quit', () => {
-  if (obsHeadless) {
-    console.log('Closing OBS...');
-    obsHeadless.kill('SIGTERM'); // Gracefully close OBS using SIGINT (same as pressing Ctrl+C)
-  }
+  OBSController.quit();
 });
